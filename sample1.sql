1cede39a9b3ef83250284f52dff994cef8e7ce91
vintage-coder <gowthamanvai@gmail.com>
2021-10-06 19:33:06 +0530

    triggers, befere update,after update,before delete,after delete commit successfully...........

--- sample1.sql
+++ sample1.sql
@@ -149,19 +149,269 @@ INSERT INTO WorkCenters(name, capacity) VALUES('Packing',200);
 
 -- -------------------After insert --------------------------------------------------
 
+DROP TABLE IF EXISTS members;
+
+CREATE TABLE members (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255),
+    birthDate DATE,
+    PRIMARY KEY (id)
+);
+
+
+DROP TABLE IF EXISTS reminders;
+
+CREATE TABLE reminders (
+    id INT AUTO_INCREMENT,
+    memberId INT,
+    message VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id , memberId)
+);
+
+DELIMITER $$
+
+CREATE TRIGGER after_members_insert
+AFTER INSERT
+ON members FOR EACH ROW
+BEGIN
+    IF NEW.birthDate IS NULL THEN
+        INSERT INTO reminders(memberId, message)
+        VALUES(new.id,CONCAT('Hi ', NEW.name, ', please update your date of birth.'));
+    END IF;
+END$$
+
+DELIMITER ;
+
+
+
+INSERT INTO members(name, email, birthDate)
+VALUES
+    ('John Doe', 'john.doe@example.com', NULL),
+    ('Jane Doe', 'jane.doe@example.com','2000-01-01');
+
+
+select * from members;
+
+select * from reminders;
+
+show triggers;
+
+-- --------------------Before update----------------------------------------------
+
+DROP TABLE IF EXISTS sales;
+
+CREATE TABLE sales (
+    id INT AUTO_INCREMENT,
+    product VARCHAR(100) NOT NULL,
+    quantity INT NOT NULL DEFAULT 0,
+    fiscalYear SMALLINT NOT NULL,
+    fiscalMonth TINYINT NOT NULL,
+    CHECK(fiscalMonth >= 1 AND fiscalMonth <= 12),
+    CHECK(fiscalYear BETWEEN 2000 and 2050),
+    CHECK (quantity >=0),
+    UNIQUE(product, fiscalYear, fiscalMonth),
+    PRIMARY KEY(id)
+);
+
+INSERT INTO sales(product, quantity, fiscalYear, fiscalMonth)
+VALUES
+    ('2003 Harley-Davidson Eagle Drag Bike',120, 2020,1),
+    ('1969 Corvair Monza', 150,2020,1),
+    ('1970 Plymouth Hemi Cuda', 200,2020,1);
+
+
+select * from sales;
+
+
+DELIMITER $$
+
+CREATE TRIGGER before_sales_update
+BEFORE UPDATE
+ON sales FOR EACH ROW
+BEGIN
+    DECLARE errorMessage VARCHAR(255);
+    SET errorMessage = CONCAT('The new quantity ',
+                        NEW.quantity,
+                        ' cannot be 3 times greater than the current quantity ',
+                        OLD.quantity);
+                        
+    IF new.quantity > old.quantity * 3 THEN
+        SIGNAL SQLSTATE '45000' 
+            SET MESSAGE_TEXT = errorMessage;
+    END IF;
+END $$
+
+DELIMITER ;
+
+UPDATE sales 
+SET quantity = 150
+WHERE id = 1;
+
+select * from sales;
+
+UPDATE sales 
+SET quantity = 500
+WHERE id = 1;
+
+show errors;
+
+show triggers;
 
 
 
 
+--  -----------------------------After update-----------------------------------------
+
+DROP TABLE IF EXISTS Sales;
+
+CREATE TABLE Sales (
+    id INT AUTO_INCREMENT,
+    product VARCHAR(100) NOT NULL,
+    quantity INT NOT NULL DEFAULT 0,
+    fiscalYear SMALLINT NOT NULL,
+    fiscalMonth TINYINT NOT NULL,
+    CHECK(fiscalMonth >= 1 AND fiscalMonth <= 12),
+    CHECK(fiscalYear BETWEEN 2000 and 2050),
+    CHECK (quantity >=0),
+    UNIQUE(product, fiscalYear, fiscalMonth),
+    PRIMARY KEY(id)
+);
+
+INSERT INTO Sales(product, quantity, fiscalYear, fiscalMonth)
+VALUES
+    ('2001 Ferrari Enzo',140, 2021,1),
+    ('1998 Chrysler Plymouth Prowler', 110,2021,1),
+    ('1913 Ford Model T Speedster', 120,2021,1);
+
+SELECT * FROM Sales;
 
 
+DROP TABLE IF EXISTS SalesChanges;
+
+CREATE TABLE SalesChanges (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    salesId INT,
+    beforeQuantity INT,
+    afterQuantity INT,
+    changedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+DELIMITER $$
+
+CREATE TRIGGER after_sales_update
+AFTER UPDATE
+ON sales FOR EACH ROW
+BEGIN
+    IF OLD.quantity <> new.quantity THEN
+        INSERT INTO SalesChanges(salesId,beforeQuantity, afterQuantity)
+        VALUES(old.id, old.quantity, new.quantity);
+    END IF;
+END$$
+
+DELIMITER ;
+
+UPDATE Sales 
+SET quantity = 350
+WHERE id = 1;
+
+SELECT * FROM SalesChanges;
+
+UPDATE Sales 
+SET quantity = CAST(quantity * 1.1 AS UNSIGNED);
+
+SELECT * FROM SalesChanges;
+
+--  -----------------------Before Delete Trigger------------------------------
+DROP TABLE IF EXISTS Salaries;
+
+CREATE TABLE Salaries (
+    employeeNumber INT PRIMARY KEY,
+    validFrom DATE NOT NULL,
+    amount DEC(12 , 2 ) NOT NULL DEFAULT 0
+);
+
+INSERT INTO salaries(employeeNumber,validFrom,amount)
+VALUES
+    (1002,'2000-01-01',50000),
+    (1056,'2000-01-01',60000),
+    (1076,'2000-01-01',70000);
+
+DROP TABLE IF EXISTS SalaryArchives;    
+
+CREATE TABLE SalaryArchives (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    employeeNumber INT ,
+    validFrom DATE NOT NULL,
+    amount DEC(12 , 2 ) NOT NULL DEFAULT 0,
+    deletedAt TIMESTAMP DEFAULT NOW()
+);
 
 
+DELIMITER $$
 
+CREATE TRIGGER before_salaries_delete
+BEFORE DELETE
+ON salaries FOR EACH ROW
+BEGIN
+    INSERT INTO SalaryArchives(employeeNumber,validFrom,amount)
+    VALUES(OLD.employeeNumber,OLD.validFrom,OLD.amount);
+END$$    
 
+DELIMITER ;
 
+DELETE FROM salaries 
+WHERE employeeNumber = 1002;
 
+SELECT * FROM SalaryArchives;    
 
+DELETE FROM salaries;
+
+SELECT * FROM SalaryArchives;
+
+-- ----------------------------After Delete Triggers---------------------------
+DROP TABLE IF EXISTS Salaries;
+
+CREATE TABLE Salaries (
+    employeeNumber INT PRIMARY KEY,
+    salary DECIMAL(10,2) NOT NULL DEFAULT 0
+);
+
+INSERT INTO Salaries(employeeNumber,salary)
+VALUES
+    (1002,5000),
+    (1056,7000),
+    (1076,8000);
+    
+DROP TABLE IF EXISTS SalaryBudgets;
+
+CREATE TABLE SalaryBudgets(
+    total DECIMAL(15,2) NOT NULL
+);
+
+INSERT INTO SalaryBudgets(total)
+SELECT SUM(salary) 
+FROM Salaries;
+
+SELECT * FROM SalaryBudgets;        
+
+CREATE TRIGGER after_salaries_delete
+AFTER DELETE
+ON Salaries FOR EACH ROW
+UPDATE SalaryBudgets 
+SET total = total - old.salary;
+
+DELETE FROM Salaries
+WHERE employeeNumber = 1002;
+
+SELECT * FROM SalaryBudgets;    
+
+DELETE FROM Salaries;
+SELECT * FROM SalaryBudgets; 
+
+show triggers;
+
+-- -------------------------------Creating Multiple triggers-----------------------
 
 
 
